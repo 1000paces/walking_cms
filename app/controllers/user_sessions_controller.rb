33ab4a6ff00fb74d4f1cbcc4aa88a0a954a4a904
Ron Miles <ron.miles@1000paces.com>
2016-05-04 15:36:11 -0400

    Fixes (incomplete) for login screen.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,7 @@
 class UserSessionsController < ApplicationController
   #before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
+  layout "tour"
 
 	def new
 		@user = User.new
